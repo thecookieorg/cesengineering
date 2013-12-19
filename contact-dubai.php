02de464ba44bf2995615c271f19750f557ad5c0a
Marko Manojlovic <marko@thecookie.org>
2013-12-18 16:05:54 -0800

    New contact page

---
+++ contact-dubai.php
@@ -0,0 +1,40 @@
+﻿<?php include('includes/head_contact.php'); ?>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+		<?php include_once("analyticstracking.php") ?>
+
+        <?php include('includes/nav_contact.php'); ?>
+
+        <div class="container">
+
+			<div class="main_content">
+			<!-- Example row of columns -->
+            <div class="row animated fadeInLeft">
+				<div class="span24 margin_main_container">
+					<img src="img/contact/dubai-main-contact-pic.png">
+					<a href="contact-vancouver.php"><img src="img/contact/link-to-vancouver-from-calgary.png"></a>
+					<a href="contact-calgary.php"><img src="img/contact/link-to-calgari-from-toronto.png"></a>
+					<a href="contact-toronto.php"><img src="img/contact/link-to-toronto-from-calgary.png"></a>
+					<a href="contact-dubai.php"><img src="img/contact/dubai-bullet-contact.png"></a>
+				</div>
+				<div class="span3 margin_main_container">
+					<img src="img/contact/dubai-right-side.png">
+					<a href="mailto:info@cesgroup.ae"><img src="img/contact/email-dubai.png"></a>
+					<a href="http://cesgroup.ae"><img src="img/contact/cesgroup-ae.png"></a>
+					<a href="http://cesengineering.ae"><img src="img/contact/cesengineering-ae.png"></a>
+					<a href="http://ecoopera.ae"><img src="img/contact/ecoopera-ae.png"></a>
+				</div>
+            </div>	
+			
+			</div>
+			
+			
+            <?php include('includes/footer-contact.php'); ?>
+
+</div> <!-- /container -->
+
+<?php include('includes/js.php'); ?>
+    </body>
+</html>
